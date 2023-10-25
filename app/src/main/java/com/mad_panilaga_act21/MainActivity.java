a1f5b3d96cab356c9adca524d21ae8fb1587f9ad
Steven Jay <90519938+PracticeStudio@users.noreply.github.com>
2023-10-25 15:04:59 +0800

    Update MainActivity.java

--- app/src/main/java/com/mad_panilaga_act21/MainActivity.java
+++ app/src/main/java/com/mad_panilaga_act21/MainActivity.java
@@ -2,15 +2,18 @@ package com.mad_panilaga_act21;
 
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
+import com.google.android.material.button.MaterialButton;
 
 public class MainActivity extends AppCompatActivity {
+
     private TextView resultTextView;
-    private String currentInput = ""; 
-    private double firstOperand = 0; // 
-    private String operator = ""; // 
+    private TextView calculationTextView;
+    private String currentInput = "";
+    private String operator = "";
+    private double firstNumber = 0;
+    private boolean clearScreen = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,106 +21,120 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         resultTextView = findViewById(R.id.result);
-        initializeButtons();
+        calculationTextView = findViewById(R.id.calculation);
+
+        MaterialButton num0 = findViewById(R.id.num0);
+        MaterialButton num1 = findViewById(R.id.num1);
+        MaterialButton num2 = findViewById(R.id.num2);
+        MaterialButton num3 = findViewById(R.id.num3);
+        MaterialButton num4 = findViewById(R.id.num4);
+        MaterialButton num5 = findViewById(R.id.num5);
+        MaterialButton num6 = findViewById(R.id.num6);
+        MaterialButton num7 = findViewById(R.id.num7);
+        MaterialButton num8 = findViewById(R.id.num8);
+        MaterialButton num9 = findViewById(R.id.num9);
+
+        MaterialButton addition = findViewById(R.id.addition);
+        MaterialButton subtraction = findViewById(R.id.subtraction);
+        MaterialButton multiplication = findViewById(R.id.multiply);
+        MaterialButton division = findViewById(R.id.divide);
+
+        MaterialButton dot = findViewById(R.id.dot);
+        MaterialButton ac = findViewById(R.id.ac);
+        MaterialButton equals = findViewById(R.id.equals);
+
+        num0.setOnClickListener(v -> onNumberClick(v));
+        num1.setOnClickListener(v -> onNumberClick(v));
+        num2.setOnClickListener(v -> onNumberClick(v));
+        num3.setOnClickListener(v -> onNumberClick(v));
+        num4.setOnClickListener(v -> onNumberClick(v));
+        num5.setOnClickListener(v -> onNumberClick(v));
+        num6.setOnClickListener(v -> onNumberClick(v));
+        num7.setOnClickListener(v -> onNumberClick(v));
+        num8.setOnClickListener(v -> onNumberClick(v));
+        num9.setOnClickListener(v -> onNumberClick(v));
+
+        addition.setOnClickListener(v -> onOperatorClick(v));
+        subtraction.setOnClickListener(v -> onOperatorClick(v));
+        multiplication.setOnClickListener(v -> onOperatorClick(v));
+        division.setOnClickListener(v -> onOperatorClick(v));
+
+        dot.setOnClickListener(v -> onDotClick(v));
+        ac.setOnClickListener(v -> onClearClick(v));
+        equals.setOnClickListener(v -> onEqualsClick(v));
     }
 
-    private void initializeButtons() {
-        // Initialize buttons by their IDs
-        Button[] buttons = {
-                findViewById(R.id.button_O),
-                findViewById(R.id.button_openbracket),
-                findViewById(R.id.button_closebracket),
-                findViewById(R.id.button_divide),
-                findViewById(R.id.button_seven),
-                findViewById(R.id.button_eight),
-                findViewById(R.id.button_nine),
-                findViewById(R.id.button_multiply),
-                findViewById(R.id.button_four),
-                findViewById(R.id.button_five),
-                findViewById(R.id.button_six),
-                findViewById(R.id.button_addition),
-                findViewById(R.id.button_one),
-                findViewById(R.id.button_two),
-                findViewById(R.id.button_three),
-                findViewById(R.id.button_minus),
-                findViewById(R.id.button_ac),
-                findViewById(R.id.button_zero),
-                findViewById(R.id.button_dot),
-                findViewById(R.id.button_equals),
-                findViewById(R.id.button_mod),
-                findViewById(R.id.button_negative),
-                findViewById(R.id.button_squareroot),
-                findViewById(R.id.button_powerofn),
-                findViewById(R.id.button_square),
-                findViewById(R.id.button_logarithmic),
-                findViewById(R.id.button_exponents),
-                findViewById(R.id.button_reciprocal),
-                findViewById(R.id.button_rootsofn)
-        };
-
-        for (Button button : buttons) {
-            setButtonClickListener(button);
+    // Function to handle number button clicks
+    public void onNumberClick(View view) {
+        if (clearScreen) {
+            clearScreen = false;
+            currentInput = "";
         }
+        String number = ((TextView) view).getText().toString();
+        currentInput += number;
+        calculationTextView.setText(currentInput);
     }
 
-    private void setButtonClickListener(Button button) {
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                handleButtonClick(button.getText().toString());
-            }
-        });
-    }
-
-    private void handleButtonClick(String buttonValue) {
-        if (isNumber(buttonValue)) {
-            currentInput += buttonValue;
-        } else if (isOperator(buttonValue)) {
-            performOperation();
-            operator = buttonValue;
-        } else if (buttonValue.equals("=")) {
-            performOperation();
-            operator = "";
-        }
-
-        updateResultTextView();
-    }
-
-    private void performOperation() {
+    // Function to handle operator button clicks
+    public void onOperatorClick(View view) {
         if (!currentInput.isEmpty()) {
-            double input = Double.parseDouble(currentInput);
+            operator = ((TextView) view).getText().toString();
+            firstNumber = Double.parseDouble(currentInput);
+            currentInput = "";
+            calculationTextView.setText(operator);
+        }
+    }
+
+    // Function to handle dot button click
+    public void onDotClick(View view) {
+        if (clearScreen) {
+            clearScreen = false;
+            currentInput = "";
+        }
+        if (!currentInput.contains(".")) {
+            currentInput += ".";
+            calculationTextView.setText(currentInput);
+        }
+    }
+
+    // Function to handle equals button click
+    public void onEqualsClick(View view) {
+        if (!currentInput.isEmpty()) {
+            double secondNumber = Double.parseDouble(currentInput);
+            double result = 0;
             switch (operator) {
                 case "+":
-                    firstOperand += input;
+                    result = firstNumber + secondNumber;
                     break;
                 case "-":
-                    firstOperand -= input;
+                    result = firstNumber - secondNumber;
                     break;
                 case "*":
-                    firstOperand *= input;
+                    result = firstNumber * secondNumber;
                     break;
                 case "/":
-                    if (input != 0) {
-                        firstOperand /= input;
+                    if (secondNumber != 0) {
+                        result = firstNumber / secondNumber;
                     } else {
                         // Handle division by zero error
+                        calculationTextView.setText("Error");
+                        clearScreen = true;
+                        return;
                     }
                     break;
-                // Add cases for other operators
             }
-            currentInput = Double.toString(firstOperand);
+            resultTextView.setText(String.valueOf(result));
+            currentInput = String.valueOf(result);
+            clearScreen = true;
         }
     }
 
-    private void updateResultTextView() {
-        resultTextView.setText(currentInput);
-    }
-
-    private boolean isNumber(String value) {
-        return value.matches("\\d") || value.equals(".");
-    }
-
-    private boolean isOperator(String value) {
-        return "+-*/".contains(value);
+    // Function to handle clear button click
+    public void onClearClick(View view) {
+        currentInput = "";
+        operator = "";
+        firstNumber = 0;
+        resultTextView.setText("");
+        calculationTextView.setText("");
     }
 }
